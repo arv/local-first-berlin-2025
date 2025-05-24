59961687e8045cfba06400419181d69bb26b56c5
Erik Arvidsson <erik.arvidsson@gmail.com>
2025-05-24 19:20:06 +0200

    Remove api for now

--- app/routes/api/push.ts
+++
@@ -1,9 +0,0 @@
-import {handlePush} from '@/server/push-handler.ts';
-import {createAPIFileRoute} from '@tanstack/react-start/api';
-
-export const APIRoute = createAPIFileRoute('/api/push')({
-  POST: async ({request}) => {
-    const response = await handlePush(request);
-    return Response.json(response);
-  },
-});
